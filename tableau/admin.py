29b903f1acf8c2f489aa4092c224191be737df61
yohan ko <yohan ko>
2016-05-20 00:52:56 +0900

    modu_tableau_2016.05.20

--- tableau/admin.py
+++ tableau/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.contrib import admin
+from .models import email
+from .models import image
+admin.site.register(email)
+admin.site.register(image)
 # Register your models here.
