403bc5b562bc8933fa77287215c11491e7a3d5e4
eureka <eureka@narusec.com>
2016-05-31 12:29:40 +0900

    파일 업로드 기능 변경. index() view를 통해 ./upload 디렉토리에 파일을 업로드함.

--- tableau/views.py
+++ tableau/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from .forms import *
-from django.http import HttpResponseRedirect
+from django.http import *
+import random
+import string
+
+def mk_random_str(length=10):
+    chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(length))
+
 
 def index(request):
+    files = []
     if request.method == "POST":
-        upimage=imageForm(request.POST)
-        if upimage.is_valid():
-            uploadimage = upimage.save(commit=False)
-            uploadimage.save()
-            return HttpResponseRedirect('/result/')
-    else:
-        upimage = imageForm()
-    return render(request, 'upload.html', {'upimage': upimage})
+        for file in request.FILES.keys():
+            fname = mk_random_str(length=30)
+            files.append(fname)
+            with open('./upload/%s' % fname, 'wb') as destination:
+                for chunk in request.FILES[file].chunks():
+                    destination.write(chunk)
+        return HttpResponse('./upload/%s uploaded!' % files)
+    return render(request, 'upload.html')
 
 def result(request):
     return render(request, 'result.html')
