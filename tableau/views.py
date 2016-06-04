15408a99f9b4282e968d1ee261703e31e55326ba
yohan ko <yohan ko>
2016-06-04 22:03:18 +0900

    result.append로 변경하였습니다.

--- tableau/views.py
+++ tableau/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
-<<<<<<< HEAD
+
 from django.http import *
 import random
 import string
 from .analyze import *
-=======
+
 from .forms import *
 from django.http import *
 import random
 import string
 
->>>>>>> bf4897fb65dbcdfd0ac550caf6e249e4d8257e45
+
 def mk_random_str(length=10):
     chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
     return ''.join(random.choice(chars) for _ in range(length))
@@ -18,6 +18,7 @@ def mk_random_str(length=10):
 
 def index(request):
     files = []
+    result = []
     if request.method == "POST":
         for file in request.FILES.keys():
             fname = mk_random_str(length=30)
@@ -25,12 +26,8 @@ def index(request):
             with open('./upload/%s' % fname, 'wb') as destination:
                 for chunk in request.FILES[file].chunks():
                     destination.write(chunk)
-<<<<<<< HEAD
-                    result=list(colorz('./upload/%s' % fname))
+                    result.append(list(colorz('./upload/%s' % fname)))
         return HttpResponse('%s uploaded!' %result)
-=======
-        return HttpResponse('./upload/%s uploaded!' % files)
->>>>>>> bf4897fb65dbcdfd0ac550caf6e249e4d8257e45
     return render(request, 'upload.html')
 
 def result(request):
